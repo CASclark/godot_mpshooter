7517794acb5ebbb5b19081e01cad14ef0f0d1f48
CASclark <lyndon.clark@cloud.collingwood-area.school.nz>
2020-06-26 23:43:19 +1200

    Bullet will disappear on impact

--- godot_mpshooter/Bullet.gd
+++ godot_mpshooter/Bullet.gd
@@ -13,3 +13,12 @@ func start(pos, dir):
 func _physics_process(delta):
 	#movement.x = speed
 	var collision = move_and_collide(movement * delta)
+	if collision:
+		if collision.collider.has_method("hit"):  #This is a function that is in the enemy script so that it will disappear etc..
+			collision.collider.hit()
+			queue_free()
+		else:
+			queue_free()
+
+func _on_VisibilityNotifier2D_screen_exited():
+	queue_free()
