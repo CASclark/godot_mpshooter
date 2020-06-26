fcc32144c3ebeb13b5b3ed15c74da75a3fdca893
CASclark <lyndon.clark@cloud.collingwood-area.school.nz>
2020-06-27 10:55:11 +1200

    gun reload and flip player according to mouse pos

--- godot_mpshooter/Scripts/Player.gd
+++ godot_mpshooter/Scripts/Player.gd
@@ -8,10 +8,20 @@ export var jumpPower = -400
 export var jetpackPower = -150
 var jetpackJuice = 100   #gas for the jetpack
 
+#setting a reload for the gun
+export var bulletFull = 5
+var bulletCount = 5
+var reload = false
+
 var Bullet = preload("res://World Tiles/Bullet.tscn")
 
 func _physics_process(delta):
 	movement.y += 10
+	
+	var mouse_pos = get_global_mouse_position()   #these are the vars to set player flip
+	var player_pos = get_position()
+	var mx = mouse_pos.x
+	var px = player_pos.x
 	
 	if Input.is_action_pressed("ui_right"):
 		movement.x = speed
@@ -37,15 +47,31 @@ func _physics_process(delta):
 	move_and_slide(movement, UP)
 	
 	if Input.is_action_just_pressed("shoot"):
-		shoot()
-	
-	$hand.look_at(get_global_mouse_position())
+		if !reload: #if there are bullets remaining
+			shoot()
+		
+	$hand.look_at(get_global_mouse_position())   #rememeber to move the center of rotation into the middle of the player
 	$gun.look_at(get_global_mouse_position())
+	
+	if mx > px: 				#flip player according to what side the mouse is
+		$Sprite.flip_h = false
+	else:
+		$Sprite.flip_h = true
+		
+	
+	if Input.is_action_just_pressed("reload"):
+		bulletCount = bulletFull
+		reload = false
+			
 		
 func shoot():
 	#Muzzle is the 2d position placed on the gun.
 	var b = Bullet.instance()
 	b.start($gun/Muzzle.global_position, $gun.rotation)
 	get_parent().add_child(b)
+	bulletCount = bulletCount -1
+	print(bulletCount)
+	if bulletCount <= 0:
+		reload = true
 	
 		
